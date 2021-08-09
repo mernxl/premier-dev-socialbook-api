27395293d9da9eee7f06cd104b38259e2caed4cd
mernxl <mernxl@gmail.com>
2021-08-09 15:52:41 +0100

    fix: ensure correct headers are sent at /posts and /users

--- src/index.js
+++ src/index.js
@@ -1,28 +1,41 @@
 const http = require('http');
 
+/*
+ * Adding right headers to responses
+ */
+
 // require will convert this into a JS object
 const users = require('./db/users.json');
 const posts = require('./db/posts.json');
 
 const PORT = 4000;
 
+
 const server = http.createServer((request, response) => {
 
   if (request.url === '/posts') {
     response.statusCode = 200;
+
+    // this will tell the client the resource media type (what we are sending)
+    response.setHeader('Content-Type', 'application/json');
+
     response.write(JSON.stringify(posts));
     return response.end();
   }
 
   if (request.url === '/users') {
     response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+
     response.write(JSON.stringify(users));
     return response.end();
   }
 
   if (request.url === '/') {
     response.statusCode = 200;
-    response.end('Hello World\n');
+    response.setHeader('Content-Type', 'text/plain');
+
+    return response.end('Hello World\n');
   }
 
   // listen to and log out errors
