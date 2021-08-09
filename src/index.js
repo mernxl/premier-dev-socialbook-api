943c1e75f1345fd82bf414601e207c9375b19cb7
mernxl <mernxl@gmail.com>
2021-08-09 15:08:07 +0100

    feat: add /posts and /users list endpoints

--- src/index.js
+++ src/index.js
@@ -4,21 +4,30 @@ const PORT = 4000;
 
 const server = http.createServer((request, response) => {
 
-  response.statusCode = 200;
+  if (request.url === '/posts') {
+    response.statusCode = 200;
+    return response.end('Post list\n');
+  }
+
+  if (request.url === '/users') {
+    response.statusCode = 200;
+    return response.end('User list\n');
+  }
+
+  if (request.url === '/') {
+    response.statusCode = 200;
+    response.end('Hello World\n');
+  }
 
   // listen to and log out errors
   response.on('error', (err => {
-    console.error(err)
-  }))
+    console.error(err);
+  }));
 
-  response.end('Hello World\n')
-})
-
-// Listen for connection events
-server.on('connection', () => {
-  console.log('New Connection')
-})
+  response.statusCode = 404;
+  response.end('Not Found\n');
+});
 
 server.listen(PORT, () => {
-  console.log(`🚀 Hurray, server listening on port ${PORT}`)
-})
+  console.log(`🚀 Hurray, server listening on port ${PORT}`);
+});
