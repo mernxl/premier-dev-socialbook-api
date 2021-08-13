0943ca23a4ab38c453e6f507c283752fa279f1a1
mernxl <mernxl@gmail.com>
2021-08-13 14:58:24 +0100

    feat: switch from http module to an express application

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-const http = require('http');
+const express = require('express');
 
 /*
  * Adding right headers to responses
  * - Request Methods (GET and POST)
+ *
+ * - switch to express
  */
 
 // require will convert this into a JS object
@@ -12,81 +14,55 @@ const posts = require('./db/posts.json');
 // heruko sets a port on the PORT env var
 const PORT = process.env.PORT || 4000;
 
+// create express application
+const app = express();
 
-const server = http.createServer((request, response) => {
-
-  // normalise the method to uppercase
-  const method = request.method.toUpperCase();
-
-  if (request.url === '/posts') {
-    response.statusCode = 200;
-
-    // this will tell the client the resource media type (what we are sending)
-    response.setHeader('Content-Type', 'application/json');
-
-    response.write(JSON.stringify(posts));
-    return response.end();
-  }
-
-  if (request.url === '/users') {
-
-    if (method === 'GET') {
-      response.statusCode = 200;
-      response.setHeader('Content-Type', 'application/json');
-
-      response.write(JSON.stringify(users));
-      return response.end();
-    }
-
-    if (method === 'POST') {
-      let body = [];
-
-      // listen to data stream from the client
-      return request.on('data', chunk => {
-        body.push(chunk);
-      })
-        .on('end', () => {
-          // convert the buffer array to a string
-          body = Buffer.concat(body).toString();
-
-          response.on('error', (err) => {
-            console.error(err);
-          });
-
-          // convert the string to an object
-          try {
-            // without this try catch block, an error in the JSON format will kill our server
-            body = JSON.parse(body); // use the object for what ever reason
-          } catch (e) {
-            response.statusCode = 500;
-            return response.end();
-          }
-
-          response.statusCode = 200;
-          response.setHeader('Content-Type', 'application/json');
-
-          response.end(JSON.stringify(body));
-        });
-    }
-
-  }
-
-  if (request.url === '/') {
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-
-    return response.end('Hello World\n');
-  }
-
-  // listen to and log out errors
-  response.on('error', (err => {
-    console.error(err);
-  }));
-
-  response.statusCode = 404;
-  response.end('Not Found\n');
+app.get('/posts', (req, res) => {
+  res.send(posts); // will stringify, set json headers, set status code
 });
 
-server.listen(PORT, () => {
+app.get('/users', (req, res) => {
+  res.send(users);
+});
+
+app.post('/users', (req, res, next) => {
+  let body = [];
+
+  // listen to data stream from the client
+  return req.on('data', chunk => {
+    body.push(chunk);
+  })
+    .on('end', () => {
+      // convert the buffer array to a string
+      body = Buffer.concat(body).toString();
+
+      // convert the string to an object
+      try {
+        // without this try catch block, an error in the JSON format will kill our server
+        body = JSON.parse(body); // use the object for what ever reason
+      } catch (e) {
+        return next(e); // will call the error handler of express
+      }
+
+      res.send(body);
+    });
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World\n');
+});
+
+app.use((req, res) => {
+  res.sendStatus(404)
+});
+
+// Error handler, first param of callback is the error passed in through next
+app.use((error, req, res, next) => {
+  console.error(error);
+
+  res.sendStatus(500)
+});
+
+app.listen(PORT, () => {
   console.log(`🚀 Hurray, server listening on port ${PORT}`);
 });
