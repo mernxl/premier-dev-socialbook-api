2f86dfbb8696c86f72df5b0063730f1e71ee1828
spykelion <ndilionel6@gmail.com>
2021-08-29 16:48:35 +0100

    redirected routes to required controllers

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
-const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
-const User  = require('../models/User');
-require('dotenv').config();
 const multer  = require('multer');
+const { login, register} = require('../controllers/auth.controller');
+
 const storage = multer.diskStorage({
   destination: (req, res, cb) => {
     cb(null, './uploads');
@@ -14,78 +12,10 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage });
 
-
-// find a user with unique id
-async function findUniqueUser(credentials = {}) {
-  const user = await User.find(credentials);
-  if (user.length > 0) {
-    console.log("found user object", user);
-    return true;
-  }
-  return false;
-}
-
 //login
-router.post('/login', async (req, res, next) => {
-  const user = await User.findOne({email: req.body.email})
-  if(user){
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if(validPassword){
-      const token = jwt.sign({email: user.email, id: user._id}, process.env.JWT_SECRET, {
-        expiresIn: "1h"
-      })
-      res.status(200).json({
-       status: 200,
-       message: "Valid user.",
-       email: user.email,
-       token: token // generate jwt now and set cookies
-      })
-    } else {
-     res.status(401).json({
-       status: 401,
-       message: "Invalid credentials" // generate jwt now and set cookies
-      })
-    }
-  } else {
-    res.status(401).json({
-      status: 401,
-      message: "Auth failed "// generate jwt now and set cookies
-     })
-  }
- 
- 
-  // post login credetectionalse
-})
+router.post('/login', login)
 
 // register
-router.post("/register", upload.single('avatar') , async (req, res, next) => {
-  // crosscheck for unique user
-  const credentials = {
-    name: req.body.name,
-    email: req.body.email,
-  };
-  if (await findUniqueUser(credentials)) {
-    res.status(501).json({
-      message: "User with name or email already exist",
-      status: 501,
-    });
-  } else {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    const user = await new User({
-      ...req.body, avatar: req.file.path,
-      password: hashedPassword,
-    });
+router.post("/register", upload.single('avatar') , register );
 
-    user
-      .save()
-      .then((result) => res.status(200).json(result))
-      .catch((err) =>
-        res.status(501).json({
-          error: "Counld'\nt save user",
-          status: 501,
-        })
-      );
-  }
-});
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,86 +1,22 @@
 const router = require("express").Router();
-const User = require("../models/User");
 const auth = require('./auth.middleware');
-
+const {
+  getAllUsers, 
+  getUserById,
+  updateUser,
+  deleteUser
+} = require('../controllers/user.controller');
 
 // get all users
-router.get("/", auth, async (req, res) => {
-  const user = await User.find({});
-  if (user.length > 0) {
-    console.log("found user object", user);
-    res.status(200).json(user);
-  } else {
-    res.status(500).json({
-      error: "Found no users",
-    });
-  }
-  // const users = await UsersC.find().toArray(); // get users cursor, convert to array
-});
+router.get("/", auth, getAllUsers);
 
 // get unique user
-router.get("/:id", auth, (req, res) => {
-  const user = User.findById(req.params.id)
-    .then((result) => {
-      res.status(200).json({
-        name: result.name,
-        username: result.username,
-        email: result.email,
-        avatar: result.avatar,
-        createdAt: result.createdAt,
-        updatedAt: result.updatedAt,
-      });
-      console.log(result);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: {
-          status: 500,
-          message: "User not found",
-        },
-      });
-    });
-});
-
-// checks if email and username exist
-
+router.get("/:id", auth, getUserById);
 
 // update a user
-router.put("/update/:id", auth, async (req, res) => {
-  let user = await User.findOne({ _id: req.params.id });
-  User.updateOne({ _id: req.params.id }, req.body)
-    .then((result) => {
-      res.status(200).json({
-        message: "User successfully updated",
-        status: 200,
-      });
-      console.log(result);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-        status: 500,
-        message: "could'\nt update user",
-      });
-    });
-});
+router.put("/update/:id", auth, updateUser);
 
 // delete a user
-router.delete("/delete/:id", auth, (req, res) => {
-  const user = User.findByIdAndDelete(req.params.id)
-    .then((result) => {
-      res.status(200).json({
-        message: "User successfully deleted",
-        status: 200,
-      });
-      console.log(result);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-        status: 500,
-        message: "could'\nt delete user",
-      });
-    });
-});
+router.delete("/delete/:id", auth, deleteUser);
 
 module.exports = router;
