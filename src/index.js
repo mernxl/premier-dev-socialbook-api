e3a389c33a872e6a8cbbec5cb6471614f94840a7
spykelion <ndilionel6@gmail.com>
2021-08-29 13:22:42 +0100

    added mongoose connect named api v1

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const ObjectID = require('mongodb').ObjectId;
+const mongoose = require('mongoose');
 
 const userRouter = require('./routes/users');
 const postRouter = require('./routes/posts');
-
-/*
+const authRouter = require('./routes/auth');
+{/*
  * Adding right headers to responses
  * - Request Methods (GET and POST)
  *
  * - switch to express
  * - add mongodb
- */
-
-// import the db, users, posts collections
-const { UsersC, PostsC } = require('./db');
+ */}
 
 // import dummy data
-require('../import-data');
+// require('../import-data');
+mongoose.connect('mongodb://localhost:27017/sb-backend', {useNewUrlParser: true, })
+ .then(res => console.log("conected..."))
+ .catch(err=>console.log("Error", err))
 
 // heruko sets a port on the PORT env var
 const PORT = process.env.PORT || 4000;
@@ -37,11 +37,13 @@ app.use(bodyParser.text());  // parses text/plain content
 // posts
 app.use('/posts', postRouter);
 // will get a particular post
-app.use('/posts/:id', postRouter);
+// app.use('/posts/:id', postRouter);
 
 app.use('/users', userRouter);
 // will get a particular user
-app.use('/users/:id', userRouter);
+// app.use('/users/:id', userRouter);
+// app.use('/users/register', userRouter);
+app.use('/api/v1/auth', authRouter);
 
 
 app.get('/', (req, res) => {
